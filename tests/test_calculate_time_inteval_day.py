56d3843b55971c81421984e58cad831c53d618b9
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-02 13:48:59 -0800

    Agrega la primera prueba fallando

---
+++ tests/test_calculate_time_inteval_day.py
@@ -0,0 +1,20 @@
+import unittest
+import datetime 
+
+class TestCalculateTimeIntervalDay(unittest.TestCase):
+    def setUp(self):
+        """
+        Crea variables que se usarán en las pruebas
+        """
+        self.date1 = datetime.datetime(2017, 6, 21, 18, 25, 30)
+        self.date2 = datetime.datetime(2017, 5, 16, 8, 21, 10)  
+
+    def test_calculate_time_interval_day(self):
+        """
+        Prueba que calcula la diferencia en días de dos fechas
+        """
+        interval = calculate_time_interval_day(self.date1 - self.date2)
+        self.assertEqual(interval, 36)
+
+if __name__ == '__main__':
+    unittest.main()
