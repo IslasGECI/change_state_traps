e84220173926b6cb2185e6466349b33fce231fce
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-02 17:55:22 -0800

    Modifica la función para que pase la segunda prueba

--- change_state_traps/calculate_time_interval_day.py
+++ change_state_traps/calculate_time_interval_day.py
@@ -1,2 +1,5 @@
-def calculate_time_interval_day(final_date, begin_date):
-    return(36)
+import datetime 
+
+def calculate_time_interval_day(final_date: datetime, begin_date: datetime):
+    delta: datetime = final_date - begin_date
+    return(delta.days)
