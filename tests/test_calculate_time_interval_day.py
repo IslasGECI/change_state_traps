6c1b8d801ea4f6b44556e7d75c4474b246499a7d
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-02 16:29:47 -0800

    Agrega archivos necesarios para correr pruebas

--- tests/test_calculate_time_interval_day.py
+++ tests/test_calculate_time_interval_day.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime 
+from change_state_traps import calculate_time_interval_day
 
 class TestCalculateTimeIntervalDay(unittest.TestCase):
     def setUp(self):
@@ -13,7 +14,7 @@ class TestCalculateTimeIntervalDay(unittest.TestCase):
         """
         Prueba que calcula la diferencia en días de dos fechas
         """
-        interval = calculate_time_interval_day(self.date1 - self.date2)
+        interval = calculate_time_interval_day(self.date1, self.date2)
         self.assertEqual(interval, 36)
 
 if __name__ == '__main__':
