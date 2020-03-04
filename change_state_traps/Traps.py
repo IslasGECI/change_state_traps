687e6b603b68bbf56112786a0afc1993222343a7
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-04 11:26:49 -0800

    Usa los decoradores de propiedades para pasar la cuarta prueba

--- change_state_traps/Traps.py
+++ change_state_traps/Traps.py
@@ -3,5 +3,13 @@ import datetime
 class Trap:
     def __init__(self, effort : int, state : str, last_change : datetime):
         self.effort : int = effort
-        self.state : str = state
+        self.__state : str = state
         self.last_change : datetime = last_change
+
+    @property
+    def state(self):
+        return(self.__state)
+
+    @state.setter
+    def state(self, new_state : str):
+        self.__state = new_state
