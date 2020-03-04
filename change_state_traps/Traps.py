001b422bca1d32050cf1b510cc4c26d22ccb4dd3
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-04 11:15:12 -0800

    Cambia la clase para que pase la tercer prueba

--- change_state_traps/Traps.py
+++ change_state_traps/Traps.py
@@ -1,4 +1,7 @@
+import datetime 
+
 class Trap:
-    def __init__(self, effort : int, state : str):
+    def __init__(self, effort : int, state : str, last_change : datetime):
         self.effort : int = effort
         self.state : str = state
+        self.last_change : datetime = last_change
