bf7b0918921db52abff0008f98c572983b32c539
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-04 10:47:30 -0800

    Modifica la prueba para revisar la propiedad `effort`

--- tests/test_Traps.py
+++ tests/test_Traps.py
@@ -4,22 +4,22 @@ from change_state_traps import *
 
 
 class MockObject:
-    def __init__(self):
-        pass
+    def __init__(self, effort: int):
+        self.effort = effort
 
 class TestTrap(unittest.TestCase):
     def setUp(self):
         """
         Crea variables que se usarán en las pruebas
         """
-        self.simulado: MockObject = MockObject() 
-        self.trampa: Trap = Trap()
+        self.simulado: MockObject = MockObject(effort = 3) 
+        self.trampa: Trap = Trap(effort = 3)
 
     def test_calculate_effort(self):
         """
         Prueba que calcula la diferencia en días de dos fechas
         """
-        self.assertEqual(self.trampa, self.simulado)
+        self.assertEqual(self.trampa.effort, self.simulado.effort)
 
 if __name__ == '__main__':
     unittest.main()
