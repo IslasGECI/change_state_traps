cdb5b3da99fb479b5356d46bdde983c1e42d3651
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-04 10:58:13 -0800

    Cambia la configuración necesaria para la segunda prueba

--- tests/test_Traps.py
+++ tests/test_Traps.py
@@ -4,20 +4,21 @@ from change_state_traps import *
 
 
 class MockObject:
-    def __init__(self, effort: int):
+    def __init__(self, effort: int, state : str):
         self.effort = effort
+        self.state : str = state
 
 class TestTrap(unittest.TestCase):
     def setUp(self):
         """
         Crea variables que se usarán en las pruebas
         """
-        self.simulado: MockObject = MockObject(effort = 3) 
-        self.trampa: Trap = Trap(effort = 3)
+        self.simulado: MockObject = MockObject(effort = 3, state = "A") 
+        self.trampa: Trap = Trap(effort = 3, state = "A")
 
-    def test_calculate_effort(self):
+    def test_verifies_effort(self):
         """
-        Prueba que calcula la diferencia en días de dos fechas
+        Verifica que los objetos de las clase `Trap` se construyan de manera correcta e su propiedad `effort`. 
         """
         self.assertEqual(self.trampa.effort, self.simulado.effort)
 
