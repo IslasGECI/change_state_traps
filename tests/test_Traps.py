9361e12831604f4392daa241fc3d5b8618d238c0
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-04 11:09:59 -0800

    Agrega la tercer prueba fallando

--- tests/test_Traps.py
+++ tests/test_Traps.py
@@ -4,17 +4,19 @@ from change_state_traps import *
 
 
 class MockObject:
-    def __init__(self, effort: int, state : str):
+    def __init__(self, effort: int, state : str, last_change : datetime):
         self.effort = effort
         self.state : str = state
+        self.last_change : datetime = last_change
 
 class TestTrap(unittest.TestCase):
     def setUp(self):
         """
         Crea variables que se usarán en las pruebas
         """
-        self.simulado: MockObject = MockObject(effort = 3, state = "A") 
-        self.trampa: Trap = Trap(effort = 3, state = "A")
+        ultimo_cambio = datetime.datetime(2020, 5, 17)
+        self.simulado: MockObject = MockObject(effort = 3, state = "A", last_change = ultimo_cambio) 
+        self.trampa: Trap = Trap(effort = 3, state = "A", last_change = ultimo_cambio)
 
     def test_verifies_effort(self):
         """
@@ -27,6 +29,12 @@ class TestTrap(unittest.TestCase):
         Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
         """
         self.assertEqual(self.trampa.state, self.simulado.state)
+
+    def test_verifies_last_change(self):
+        """
+        Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
+        """
+        self.assertEqual(self.trampa.last_change, self.simulado.last_change)
 
 if __name__ == '__main__':
     unittest.main()
