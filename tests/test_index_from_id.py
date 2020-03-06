a53d32cbadfe1ad20265d0d182ee697eea04c7d5
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-06 13:57:04 -0800

    Agrega la segunda prueba fallando

--- tests/test_index_from_id.py
+++ tests/test_index_from_id.py
@@ -6,15 +6,17 @@ class TestIndexFromId(unittest.TestCase):
         """
         Crea variables que se usarán en las pruebas
         """
-        self.Ids: list = ['id1']
-        self.index: list = [2]
+        self.Ids: list = ['id1', 'id2']
+        self.index: list = [2, 4]
 
     def test_make_dict(self):
         """
         Verifica que la función `make_dict_from_id_index` genere el diccionario correcto. 
         """
-        dictionary : dict = make_dict_from_id_index(self.index, self.id)
+        dictionary : dict = make_dict_from_id_index(self.index[0], self.Ids[0])
         self.assertEqual(dictionary[self.Ids[0]], self.index[0])
+        dictionary : dict = make_dict_from_id_index(self.index[1], self.Ids[1])
+        self.assertEqual(dictionary[self.Ids[1]], self.index[1])
 
 if __name__ == '__main__':
     unittest.main()
