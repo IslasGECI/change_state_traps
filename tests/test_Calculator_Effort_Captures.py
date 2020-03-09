ad2addf0e64fbf2368f543523faeaec0c8b2744d
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-09 16:47:45 -0700

    Agrega la segunda prueba fallando

--- tests/test_Calculator_Effort_Captures.py
+++ tests/test_Calculator_Effort_Captures.py
@@ -9,12 +9,18 @@ class TestCalculatorEffortCaptures(unittest.TestCase):
         Crea variables que se usarán en las pruebas
         """
         self.Esfuerzo_Capturas : Calculator_Effort_Captures = Calculator_Effort_Captures()
+        ultimo_cambio : datetime.datetime = datetime.datetime(2020, 5, 17)
+        self.dictionary : dict = {"effort" : [30, 40], "state" : ["A", "A"], "last_change" : [ultimo_cambio, ultimo_cambio]}
+        self.dataframe : pd.DataFrame = pd.DataFrame.from_dict(self.dictionary)
 
     def test_initialization(self):
         """
         Verifica que los objetos de las clase `Calculator_Effort_Captures` se puedan inicializar.
         """
-        self.assertEqual(self.Eafuerzo_Capturas , Calculator_Effort_Captures)
+        self.assertEqual(self.Esfuerzo_Capturas , Calculator_Effort_Captures)
+        self.Esfuerzo_Capturas.init(self.dataframe)
+        self.assertEqual(self.Esfuerzo_Capturas.effort , sum(self.dictionary['effort']))
+
 
 if __name__ == '__main__':
     unittest.main()
