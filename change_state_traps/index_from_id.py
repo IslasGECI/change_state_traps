c54197f59fe05a3aff5a5cf1e9030b1eda2cda94
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-09 13:51:04 -0700

    Modifica la función para que pase la prueba

--- change_state_traps/index_from_id.py
+++ change_state_traps/index_from_id.py
@@ -5,4 +5,7 @@ def make_dict_from_id_index(ids : str, index: int):
     return(dictionary)
 
 def dictionary_for_initialization(initialization: pd.DataFrame):
-    pass
+    index_from_id = {}
+    for index, row in initialization.iterrows():
+        index_from_id.update({row['id']: index})
+    return(index_from_id)
