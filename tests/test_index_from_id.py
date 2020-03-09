b5560a14bee15b8eb9944b76e84caba6717f92f7
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-09 13:11:02 -0700

    Agrega la primera prueba para nueva función

--- tests/test_index_from_id.py
+++ tests/test_index_from_id.py
@@ -8,6 +8,12 @@ class TestIndexFromId(unittest.TestCase):
         """
         self.Ids: list = ['id1', 'id2']
         self.index: list = [2, 4]
+        self.effort: list = [25, 36]
+        self.state: list = ["A", "D"]
+        self.dictionary: dict = {'id2': 4, 'id1': 2}
+        self.data_frame : pd.DataFrame = pd.DataFrame({'id': self.Ids,
+        'effort': self.effort,
+        'state': self.state})
 
     def test_make_dict(self):
         """
@@ -17,6 +23,13 @@ class TestIndexFromId(unittest.TestCase):
         self.assertEqual(dictionary[self.Ids[0]], self.index[0])
         dictionary : dict = make_dict_from_id_index(self.Ids[1], self.index[1])
         self.assertEqual(dictionary[self.Ids[1]], self.index[1])
+
+    def test_dictionary_for_initialization(self):
+        """
+        Verifica que se generan los diccionarios para inicializar el cambiador
+        """
+        ids_and_index = dictionary_for_initialization(self.data_frame)
+        self.assertEqual(ids_and_index[self.Ids[1]], self.index[1])
 
 if __name__ == '__main__':
     unittest.main()
