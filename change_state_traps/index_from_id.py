1c20215d4cb18e4795cce06cd59ae14e0bfb68fb
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-09 13:11:16 -0700

    Agrega segunda función vacía

--- change_state_traps/index_from_id.py
+++ change_state_traps/index_from_id.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
 def make_dict_from_id_index(ids : str, index: int):
     dictionary : dict = {ids : index}
     return(dictionary)
+
+def dictionary_for_initialization(initialization: pd.DataFrame):
+    pass
