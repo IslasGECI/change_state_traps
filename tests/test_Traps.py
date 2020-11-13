0d157b3f09710058f18542abebaab0c067092aab
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-11-12 17:44:18 -0800

    ✅ Cambia las pruebas para utilizar pytest y no unittest

--- tests/test_Traps.py
+++ tests/test_Traps.py
@@ -13,47 +13,46 @@ class MockObject:
         self.state: str = state
 
 
-class TestTrap(unittest.TestCase):
-    def setUp(self):
-        """
-        Crea variables que se usarán en las pruebas
-        """
-        ultimo_cambio = datetime.datetime(2020, 5, 17)
-        argumentos = {"effort": 3, "state": "A", "last_change": ultimo_cambio}
-        self.simulado: MockObject = MockObject(**argumentos)
-        self.trampa: Trap = Trap(**argumentos)
+ultimo_cambio = datetime.datetime(2020, 5, 17)
+argumentos = {"effort": 3, "capture": 2, "state": "A", "last_change": ultimo_cambio}
+simulado: MockObject = MockObject(**argumentos)
+trampa: Trap = Trap(**argumentos)
 
-    def test_verifies_effort(self):
-        """
-        Verifica que los objetos de las clase `Trap` se construyan de manera correcta e su propiedad `effort`. 
-        """
-        self.assertEqual(self.trampa.effort, self.simulado.effort)
+def test_verifies_capture():
+    """
+    Verifica que los objetos de las clase `Trap` se construyan de manera correcta e su propiedad `effort`. 
+    """
+    assert trampa.capture == simulado.capture
 
-    def test_verifies_state(self):
-        """
-        Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
-        """
-        self.assertEqual(self.trampa.state, self.simulado.state)
+def test_verifies_effort():
+    """
+    Verifica que los objetos de las clase `Trap` se construyan de manera correcta e su propiedad `effort`. 
+    """
+    assert trampa.effort == simulado.effort
 
-    def test_verifies_last_change(self):
-        """
-        Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
-        """
-        self.assertEqual(self.trampa.last_change, self.simulado.last_change)
+def test_verifies_state():
+    """
+    Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
+    """
+    assert trampa.state == simulado.state
 
-    def test_update_trap(self):
-        """
-        Verifica que los objetos de las clase `Trap` se puedan actualizar cuando exista un cambio en su estado.
-        """
-        self.trampa.update(new_state="D", new_date=datetime.datetime(2020, 5, 20))
-        self.assertEqual(self.trampa.state, "D")
-        self.assertEqual(self.trampa.last_change, datetime.datetime(2020, 5, 20))
-        self.assertEqual(self.trampa.effort, self.simulado.effort + 3)
-        self.trampa.update(new_state="A", new_date=datetime.datetime(2020, 5, 27))
-        self.assertEqual(self.trampa.effort, self.simulado.effort + 3)
-        self.trampa.update(new_state="X", new_date=datetime.datetime(2020, 5, 29))
-        self.assertEqual(self.trampa.effort, self.simulado.effort + 5)
+def test_verifies_last_change():
+    """
+    Verifica que los objetos de las clase `Trap` se construyan de manera correcta en su propiedad `state`. 
+    """
+    assert trampa.last_change == simulado.last_change
+
+def test_update_trap():
+    """
+    Verifica que los objetos de las clase `Trap` se puedan actualizar cuando exista un cambio en su estado.
+    """
+    trampa.update(new_state="D", new_date=datetime.datetime(2020, 5, 20))
+    assert trampa.state == "D"
+    assert trampa.last_change == datetime.datetime(2020, 5, 20)
+    assert trampa.effort == simulado.effort + 3
+    trampa.update(new_state="A", new_date=datetime.datetime(2020, 5, 27))
+    assert trampa.effort == simulado.effort + 3
+    trampa.update(new_state="X", new_date=datetime.datetime(2020, 5, 29))
+    assert trampa.effort == simulado.effort + 5
 
 
-if __name__ == "__main__":
-    unittest.main()
